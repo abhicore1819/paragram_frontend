85f23688796d32ff6fc6455d3dfcd11e5bac29aa
Abhinav Chaudhary <abhicore1819@gmail.com>
2026-07-15 14:56:20 +0530

    testing api

--- paragram_frontend/src/service/FetchFeed.js
+++ paragram_frontend/src/service/FetchFeed.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 const FEED_URL = import.meta.env.VITE_FETCH_FEED;
 const FetchFeed = async () => {
+  console.log("backend url:-", FEED_URL);
   try {
     const feed_request = await axios.get(FEED_URL);
+    console.log("backend url:-", FEED_URL);
     const feed_response = feed_request.data;
-    return feed_response
+    console.log("response:-", feed_response);
+    return feed_response;
   } catch (error) {
     console.log("Error occured:- ", error);
   }
